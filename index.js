56db2e3d06e21ec6e5ec6383ab9e00e714a1769c
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-09 12:56:09 +0100

    Add country field validation and error msg.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const formValidation = (function () {
   const emailError = document.querySelector("#email + span.error");
 
   email.addEventListener("input", (event) => {
-    email.validity.valid ? (emailError.textContent = "") : showError();
+    email.validity.valid ? (emailError.textContent = "") : showEmailError();
   });
 
-  function showError() {
+  function showEmailError() {
     if (email.validity.valueMissing) {
       emailError.textContent = "Please enter an email address.";
     } else if (email.validity.typeMismatch) {
@@ -16,4 +16,13 @@ const formValidation = (function () {
         "Entered value needs to be an email address (ex. tony@starkindustries.com).";
     }
   }
+
+  const country = form.querySelector("#country");
+  const countryErr = form.querySelector("#country + span.error");
+
+  country.addEventListener("input", (event) => {
+    country.value !== "none"
+      ? (countryErr.textContent = "")
+      : (countryErr.textContent = "Please select a country from the list.");
+  });
 })();
