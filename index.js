103958d94291343751f1e5b3628a0490fda516f3
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-09 14:43:31 +0100

    Add zip code validation and error per country.

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const formValidation = (function () {
   const form = document.querySelector("form");
 
   const email = form.querySelector("#email");
-  const emailError = document.querySelector("#email + span.error");
+  const emailErr = document.querySelector("#email + span.error");
 
   email.addEventListener("input", (event) => {
-    email.validity.valid ? (emailError.textContent = "") : showEmailError();
+    email.validity.valid ? (emailErr.textContent = "") : showEmailError();
   });
 
   function showEmailError() {
     if (email.validity.valueMissing) {
-      emailError.textContent = "Please enter an email address.";
+      emailErr.textContent = "Please enter an email address.";
     } else if (email.validity.typeMismatch) {
-      emailError.textContent =
+      emailErr.textContent =
         "Entered value needs to be an email address (ex. tony@starkindustries.com).";
     }
   }
@@ -25,4 +25,41 @@ const formValidation = (function () {
       ? (countryErr.textContent = "")
       : (countryErr.textContent = "Please select a country from the list.");
   });
+
+  const zip = form.querySelector("#zip");
+  const zipErr = form.querySelector("#zip + span.error");
+
+  zip.addEventListener("input", (event) => {
+    checkZip();
+  });
+
+  function checkZip() {
+    if (country.value === "none") {
+      zipErr.textContent = "Please select a country from the list above.";
+    } else {
+      showZipError();
+    }
+
+    function showZipError() {
+      const constraints = {
+        ca: [
+          "^(CA-)?\\w\\d\\w\\d\\w\\d$",
+          "Canadian ZIPs must have 3 letters and 3 digits in an alternating order: ex. N3Y4K1",
+        ],
+        nl: [
+          "^(NL-)?\\d{4}\\w{2}$",
+          "Dutch ZIPs must have 4 digits, followed by 2 letters: ex. 3581MS",
+        ],
+      };
+      const selectedCountry = country.value;
+      const constraint = new RegExp(constraints[selectedCountry][0], "");
+
+      if (constraint.test(zip.value)) {
+        console.log("match");
+        zipErr.textContent = "";
+      } else {
+        zipErr.textContent = constraints[selectedCountry][1];
+      }
+    }
+  }
 })();
